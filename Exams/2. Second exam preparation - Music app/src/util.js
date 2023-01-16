ac242190ba1a34211e95023ce72d996b403e81c2
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-01-16 09:24:38 +0200

    Completed task: Created util.js

---
+++ Exams/2. Second exam preparation - Music app/src/util.js
@@ -0,0 +1,26 @@
+import { get } from './api/api.js';
+
+export async function getUserData(){
+    let user = JSON.parse(sessionStorage.getItem('userData'));
+    return user;
+}
+
+export async function setUserData(data){
+    sessionStorage.setItem("userData", JSON.stringify(data));
+}
+
+export async function clearUserData(){
+    sessionStorage.removeItem("userData");
+}
+
+export async function getAll(){
+    return get("/data/albums?sortBy=_createdOn%20desc&distinct=name");
+}
+
+export async function getById(id){
+    return get("/data/albums/" + id);
+}
+
+// export async function deleteById(id){
+//     return del('/data/pets/' + id);
+// }
