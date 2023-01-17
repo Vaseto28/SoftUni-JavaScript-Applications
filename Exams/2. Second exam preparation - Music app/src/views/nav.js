d3d4da068fd8ea7ec1a9664ad4f5e4820c32b90c
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-01-17 21:25:18 +0200

    Completed task: Created active navigation

---
+++ Exams/2. Second exam preparation - Music app/src/views/nav.js
@@ -0,0 +1,34 @@
+import { html, render, page } from "../lib.js";
+import { getUserData } from "../util.js";
+import { logout } from "../api/user.js"
+
+let navParent = document.querySelector('header');
+
+export async function updateNav(){
+    let user = Boolean(await getUserData());
+
+    let navTemplate = (user, onLogout) => html`
+    <nav>
+        <img src="./images/headphones.png">
+        <a href="/">Home</a>
+        <ul>
+            <!--All user-->
+            <li><a href="/catalog">Catalog</a></li>
+            <li><a href="/search">Search</a></li>
+            ${user ? html`
+            <li><a href="/create">Create Album</a></li>
+            <li><a @click=${onLogout} href="javascript:void(0)">Logout</a></li>` 
+                : html`
+            <li><a href="/login">Login</a></li>
+            <li><a href="/register">Register</a></li>`}
+        </ul>
+    </nav>`;
+
+    render(navTemplate(user, onLogout), navParent);
+}
+
+function onLogout(){
+    logout();
+    updateNav();
+    page.redirect('/');
+}
