913fc040b2efb2bdfd426d4a420564646cb7a287
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-01-19 09:23:46 +0200

    Completed task: Created home view

---
+++ Exams/3. Third exam preparation - Sole mates/src/views/home.js
@@ -0,0 +1,15 @@
+import { html } from '../lib.js';
+import { updateNav } from './nav.js';
+
+let homeTemplate = () => html`
+<section id="home">
+    <h1>Welcome to Sole Mates</h1>
+    <img src="./images/home.jpg" alt="home" />
+    <h2>Browse through the shoe collectibles of our users</h2>
+    <h3>Add or manage your items</h3>
+  </section>`;
+
+export function showHome(ctx){
+    ctx.render(homeTemplate());
+    updateNav();
+}
