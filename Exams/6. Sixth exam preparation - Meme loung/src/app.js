fc880b4d1fec01944fee176c3077065d84f5d83b
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-01-24 01:55:23 +0200

    Completed task: Created app.js

---
+++ Exams/6. Sixth exam preparation - Meme loung/src/app.js
@@ -0,0 +1,42 @@
+import { page, render } from './lib.js';
+import { getUserData } from './util.js';
+import { showAllMemes } from './views/allMemes.js';
+import { showCreate } from './views/create.js';
+import { showDetails } from './views/details.js';
+import { showEdit } from './views/edit.js';
+import { showHome } from './views/home.js';
+import { showLogin } from './views/login.js';
+import { updateNav } from './views/nav.js';
+import { showRegister } from './views/register.js';
+import { showUserProfile } from './views/userProfile.js';
+
+let main = document.querySelector('main');
+
+page(decorateCtx);
+page('/', showHome)
+page('/allMemes', showAllMemes);
+page('/login', showLogin);
+page('/register', showRegister);
+page('/myProfile', showUserProfile);
+page('/create', showCreate);
+page('/details/:id', showDetails);
+page('/edit/:id', showEdit);
+
+updateNav();
+page.start();
+
+async function decorateCtx(ctx, next){
+    ctx.render = renderMain;
+    ctx.updateNav = updateNav;
+    
+    const user = await getUserData();
+    if (user){
+        ctx.user = user;
+    }
+
+    next();
+}
+
+function renderMain(content){
+    render(content, main);
+}
