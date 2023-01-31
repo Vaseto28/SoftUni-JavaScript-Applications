f217025f581fb188dbd99d56fbe1f80f118c5282
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-01-31 08:18:22 +0200

    Completed task: Created login view

---
+++ Exams/Regular exam - Music gallery/src/views/login.js
@@ -0,0 +1,36 @@
+import { login } from '../api/user.js';
+import { html } from '../lib.js';
+import { updateNav } from './nav.js';
+
+let loginTemplate = (onLogin) => html`
+<section id="login">
+    <div class="form">
+        <h2>Login</h2>
+        <form @submit=${onLogin} class="login-form">
+            <input type="text" name="email" id="email" placeholder="email" />
+            <input type="password" name="password" id="password" placeholder="password" />
+            <button type="submit">login</button>
+            <p class="message">
+                Not registered? <a href="/register">Create an account</a>
+            </p>
+        </form>
+    </div>
+</section>`;
+
+export function showLogin(ctx){
+    ctx.render(loginTemplate(onLogin));
+
+    async function onLogin(e){
+        e.preventDefault();
+        let formData = new FormData(e.target);
+        let {email, password} = Object.fromEntries(formData);
+
+        if (!email || !password){
+            return alert('All fields are required!');
+        }
+
+        await login(email, password);
+        updateNav();
+        ctx.page.redirect('/catalog');
+    }
+}
