c744ea97040d3a5f2bc2c0b43c15e6e0eb3a124d
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-01-31 08:16:03 +0200

    Completed task: Created util.js file

---
+++ Exams/Regular exam - Music gallery/src/util.js
@@ -0,0 +1,30 @@
+import { get } from './api/api.js';
+
+export async function getUserData(){
+    let user = JSON.parse(sessionStorage.getItem('userData'));
+    return user;
+}
+
+export async function setUserData(data){
+    sessionStorage.setItem("userData", JSON.stringify(data));
+}
+
+export async function clearUserData(){
+    sessionStorage.removeItem("userData");
+}
+
+export async function getAll(){
+    return get("/data/albums?sortBy=_createdOn%20desc");
+}
+
+export async function getById(id){
+    return get("/data/albums/" + id);
+}
+
+export async function getAllMyPosts(userId){
+    return get(`/data/posts?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`);
+}
+
+// export async function deleteById(id){
+//     return del('/data/pets/' + id);
+// }
